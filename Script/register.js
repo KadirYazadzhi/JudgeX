e7a687475414bf56d0a40abfa4e85164d1e1341a
kadir7563483 <148765216+TheTrackOne@users.noreply.github.com>
2024-06-05 13:33:17 +0300

    new

--- Script/register.js
+++ Script/register.js
@@ -1,19 +1,26 @@
 class RegisterToggle {
-    constructor(buttonId, menuClass) {
+    constructor(buttonId, menuSelector, closeButtonId) {
         this.button = document.getElementById(buttonId);
-        this.form = document.querySelector(menuClass);
+        this.form = document.querySelector(menuSelector);
+        this.close = document.getElementById(closeButtonId);
         this.init();
     }
 
     init() {
         this.button.addEventListener('click', () => this.toggleMenu());
+        this.close.addEventListener('click', () => this.closeElement());
+
     }
 
     toggleMenu() {
         this.form.classList.toggle('openForm');
     }
+
+    closeElement() {
+        this.form.classList.remove('openForm');
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    new RegisterToggle('phone-user-icon', '.register-form-box');
+    new RegisterToggle('phone-user-icon', '.register-form-box', 'close-register');
 });
