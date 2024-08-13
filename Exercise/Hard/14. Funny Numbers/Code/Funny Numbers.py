f0925c27fae0f29022ee3da007878afe50f34ecc
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-08-13 17:58:25 +0300

    new

--- Exercise/Hard/14. Funny Numbers/Code/Funny Numbers.py
+++
@@ -1,9 +0,0 @@
-def funny_number(n, p):
-    digits = [int(digit) for digit in str(n)]
-    sum_digits = sum(digit ** (p + i) for i, digit in enumerate(digits))
-    return sum_digits // n if sum_digits % n == 0 else -1
-
-n = int(input())
-p = int(input())
-
-print(funny_number(n, p))
