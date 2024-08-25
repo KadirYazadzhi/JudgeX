e5bbe32de5718f52a85dffadbcf0cb75dd431cd6
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-08-26 01:50:31 +0300

    Update 404.js

--- Script/404.js
+++ Script/404.js
@@ -1,18 +1,41 @@
-var str = document.getElementsByTagName('div')[0].innerHTML.toString();
-var i = 0;
-document.getElementsByTagName('div')[0].innerHTML = "";
+class TextAnimator {
+    constructor(selector, animationSpeed = 10, redirectDelay = 15000, redirectUrl = 'index.html') {
+        this.element = document.querySelector(selector);
+        this.fullText = this.element.innerHTML.toString();
+        this.animationSpeed = animationSpeed;
+        this.redirectDelay = redirectDelay;
+        this.redirectUrl = redirectUrl;
+        this.currentIndex = 0;
 
-setTimeout(function() {
-    var se = setInterval(function() {
-        i++;
-        document.getElementsByTagName('div')[0].innerHTML = str.slice(0, i) + "|";
-        if (i === str.length) {
-            clearInterval(se);
-            document.getElementsByTagName('div')[0].innerHTML = str;
-        }
-    }, 10);
-},0);
+        this.element.innerHTML = "";
+        this.init();
+    }
 
-setTimeout(() => {
-    window.location.href = 'index.html';
-}, 5000);
+    init() {
+        this.animateText();
+        this.scheduleRedirect();
+    }
+
+    animateText() {
+        setTimeout(() => {
+            const intervalId = setInterval(() => {
+                this.currentIndex++;
+                this.element.innerHTML = this.fullText.slice(0, this.currentIndex) + "|";
+
+                if (this.currentIndex === this.fullText.length) {
+                    clearInterval(intervalId);
+                    this.element.innerHTML = this.fullText;
+                }
+            }, this.animationSpeed);
+        }, 0);
+    }
+
+    scheduleRedirect() {
+        setTimeout(() => {
+            window.location.href = this.redirectUrl;
+        }, this.redirectDelay);
+    }
+}
+
+new TextAnimator('div', 10, 15000, 'index.html');
+
