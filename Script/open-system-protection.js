eacae6f1e165222dc90380a31fe4f9e6b1d72807
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-08-29 12:05:07 +0300

    new

--- Script/open-system-protection.js
+++ Script/open-system-protection.js
@@ -1,6 +1,26 @@
-if (localStorage.getItem('selectedCardIndex') === null) {
-    window.location.href = "index.html#languages";
+class NavigationManager {
+    constructor() {}
+    
+    navigate() {
+        const selectedLevel = getSelectedLevel();
+        const selectedLanguage = getSelectedLanguage();
+
+        if (selectedLevel === null && selectedLanguage === null) {
+            this.redirect("index.html");
+        } else if (selectedLanguage === null) {
+            this.redirect("index.html#languages");
+        } else if (selectedLevel === null) {
+            this.redirect("index.html#difficult");
+        }
+    }
+
+    redirect(url) {
+        window.location.href = `${url}`;
+    }
 }
-if (localStorage.getItem('selectedButton') === null) {
-    window.location.href = "index.html#difficult";
-}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navigationManager = new NavigationManager('');
+    navigationManager.navigate();
+});
+
