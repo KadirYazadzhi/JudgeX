158d650f7ab4d7dc6a9436be44cca952925dd367
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-09-07 02:04:43 +0300

    new

--- Script/Profile/user-details.js
+++ Script/Profile/user-details.js
@@ -1,8 +1,42 @@
-const profileUsernameField = document.getElementById('profile-username');
-const profileImage = document.getElementById('profile-image');
-const diamondField = document.getElementById('diamond-availability');
+// Class to manage user profile details
+class UserProfile {
+    constructor() {
+        this.profileUsernameField = document.getElementById('profile-username');
+        this.profileImage = document.getElementById('profile-image');
+        this.diamondField = document.getElementById('diamond-availability');
+        this.updateProfile();
+    }
 
-profileUsernameField.innerHTML = '@' + getUsernameValue();
-profileImage.src = 'Image/icon-user-1.png';
-diamondField.innerHTML = `${getDiamond()} <i class="fa-solid fa-gem"></i>`
+    /**
+     * Update the profile details including username, image, and diamond count
+     */
+    updateProfile() {
+        this.setUsername();
+        this.setProfileImage();
+        this.setDiamondCount();
+    }
 
+    /**
+     * Set the username in the profile
+     */
+    setUsername() {
+        this.profileUsernameField.innerHTML = '@' + getUsernameValue();
+    }
+
+    /**
+     * Set the profile image source
+     */
+    setProfileImage() {
+        this.profileImage.src = 'Image/icon-user-1.png';
+    }
+
+    /**
+     * Set the diamond count in the profile
+     */
+    setDiamondCount() {
+        this.diamondField.innerHTML = `${getDiamond()} <i class="fa-solid fa-gem"></i>`;
+    }
+}
+
+// Initialize the UserProfile class when the script runs
+new UserProfile();
