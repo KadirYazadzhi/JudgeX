80c7ed5542a970127470d506bcb3f548e779a186
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-10-03 09:41:46 +0300

    new

--- Script/Profile/function-for-certificate-image.js
+++ Script/Profile/function-for-certificate-image.js
@@ -44,6 +44,42 @@ class ImageToPDFConverter {
     }
 }
 
+class FullscreenImageHandler {
+    constructor(expandButtonSelector, imageSelector, fullscreenContainerSelector, closeButtonSelector) {
+        this.expandButton = document.querySelector(expandButtonSelector);
+        this.imageElement = document.querySelector(imageSelector);
+        this.fullscreenContainer = document.querySelector(fullscreenContainerSelector);
+        this.fullscreenImage = document.querySelector(`${fullscreenContainerSelector} img`);
+        this.closeButton = document.querySelector(closeButtonSelector);
+
+        // Add event listener to the expand button
+        this.expandButton.addEventListener('click', () => this.openFullscreen());
+
+        // Add event listener to the close button
+        this.closeButton.addEventListener('click', () => this.closeFullscreen());
+    }
+
+    // Open the fullscreen modal
+    openFullscreen() {
+        // Set the source of the fullscreen image to match the clicked image
+        this.fullscreenImage.src = this.imageElement.src;
+
+        // Display the fullscreen container
+        this.fullscreenContainer.style.display = 'flex';
+    }
+
+    // Close the fullscreen modal
+    closeFullscreen() {
+        // Hide the fullscreen container
+        this.fullscreenContainer.style.display = 'none';
+    }
+}
+
+// Initialize the FullscreenImageHandler when DOM is ready
+document.addEventListener("DOMContentLoaded", function() {
+    new FullscreenImageHandler('.fa-expand', '.users-certificate-img', '#fullscreenContainer', '#closeBtn');
+});
+
 // Initialize the ImageToPDFConverter class when DOM is ready
 document.addEventListener("DOMContentLoaded", function() {
     new ImageToPDFConverter('.users-certificate-img', '.fa-file-arrow-down');
