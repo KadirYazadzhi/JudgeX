fd855fe32038d7452f66a3bd56782185e5fcd5d6
Kadir Yazadzhi <smx590751@gmail.com>
2024-12-31 02:06:41 +0200

    Update chat-bot-container.js

--- Script/For All/chat-bot-container.js
+++ Script/For All/chat-bot-container.js
@@ -1,9 +1,14 @@
 const chatBotContainer = document.getElementById("chat-bot");
+const messageContainer = document.querySelector(".messages-area");
 const chatBotCloseButton = document.querySelector(".close i");
 const chatBotOpenButton = document.querySelector(".extra-button-2");
 
 chatBotOpenButton.addEventListener("click", function() {
     setTimeout(() => chatBotContainer.classList.add("open-chat-container"), 200);
+
+    if (messageContainer.textContent !== "") return;
+
+    app.startingMessageFromBot(); // Call the method to send the first message
 });
 
 chatBotCloseButton.addEventListener("click", function() {
