2b4a71138eb1850084dfaf950499523e695b46c2
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-03-17 08:58:32 +0200

    Update leaderboard.js

--- Script/Home/leaderboard.js
+++ Script/Home/leaderboard.js
@@ -1,2 +1,31 @@
+if (window.innerWidth < 768) {
+    document.addEventListener("DOMContentLoaded", function () {
+        const cards = document.querySelectorAll(".leaderboard-card");
+        console.log(cards);
+        const dots = document.querySelectorAll(".leaderboard-dot");
+        console.log(dots);
 
+        let activeIndex = 1;
 
+        function updateActiveCard() {
+            cards.forEach((card, index) => {
+                card.style.display = index === activeIndex ? "flex" : "none";
+            });
+
+            dots.forEach((dot, index) => {
+                dot.classList.toggle("active", index === activeIndex);
+                console.log(index)
+            });
+        }
+
+        dots.forEach((dot, index) => {
+            dot.addEventListener("click", function () {
+                activeIndex = index;
+                updateActiveCard();
+            });
+        });
+
+        updateActiveCard();
+    });
+
+}
